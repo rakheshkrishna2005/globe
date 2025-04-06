a18374f5c5b450ed7b42c87c74a472314212bf0b
rakheshkrishna2005 <rakheshkrishnap@gmail.com>
2025-04-07 00:49:54 +0530

    title updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 import Globe from '@/components/Globe';
 import { Sparkles } from '@/components/Sparkles';
 import BackgroundMusic from '@/components/BackgroundMusic';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-black-900">
-      {/* Render Globe */}
-      <Globe />
+    <>
+      <Head>
+        <title>Glowbe - Interstellar Experience</title>
+      </Head>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-black-900">
+        {/* Render Globe */}
+        <Globe />
 
       {/* Render Sparkles as a background or overlay */}
       <div className="absolute inset-0 z-[2] w-screen h-screen overflow-hidden pt-16 pb-16 [mask-image:radial-gradient(100%_100%,white,transparent)] before:absolute before:inset-0 before:bg-[radial-gradient(circle_at_center,#3273ff,transparent_90%)] before:opacity-40 after:absolute">
@@ -26,5 +31,6 @@ export default function Home() {
         autoPlay={true}
       />
     </div>
+    </>
   );
 }
